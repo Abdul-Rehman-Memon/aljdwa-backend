90f0fe92f66ecd87159d5f192a199dc6b26e18c6
Waris Raza <warisrazaarain@gmail.com>
2024-11-17 02:34:42 +0500

    updated entrepreneur details ->added further columns

--- database/migrations/2024_10_18_195404_create_entrepreneur_details_table.php
+++ database/migrations/2024_10_18_195404_create_entrepreneur_details_table.php
@@ -19,14 +19,18 @@ return new class extends Migration
             $table->foreignUuid('user_id')->constrained('users')
             ->cascadeOnUpdate()
             ->cascadeOnDelete();
-            $table->string('position');
-            $table->string('major');
-            $table->text('resume');
+            $table->string('position')->nullable();
+            $table->string('major')->nullable();
+            $table->text('resume')->nullable();
             $table->string('website');
             $table->text('project_description');
             $table->text('problem_solved');
             $table->text('solution_offering');
             $table->text('previous_investment');
+            $table->text('company_registered');
+            $table->text('saudi_vision_alignment');
+            $table->text('positive_impact');
+            $table->text('help_needed');
             $table->text('industry_sector');
             $table->text('business_model');
             $table->text('patent')->nullable();
